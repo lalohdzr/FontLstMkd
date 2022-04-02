f50792b8e620ef920f92369ec1c5fa0a82fee066
lalohdzr <lalohdzr@hotmail.com>
2022-04-01 21:29:47 -0600

    Proyecto lst supermercado frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ProyectoLstMkd';
+  title = 'Proyecto Lst Mkd';
+  msgSave = 'Articulo Agregado';
+  msgBorrado = 'Articulo Eliminado';
+  msgLst = 'Listado Borrado';
+  name = 'nicolas';
+
+  lstArt = [];
+  nwArt = '';
+
+  changeTitle(){
+    this.title = 'Nuevo titulo '; 
+  }
+  
+  addArt(){
+    this.lstArt.push(this.nwArt);
+    this.nwArt = '';
+  }
 }
